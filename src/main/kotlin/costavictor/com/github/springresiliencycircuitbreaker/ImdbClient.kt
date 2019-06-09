4a45a59d4ccc1784b7234d5b9c5ac95fe9c8a778
augustovictor <victoraweb@gmail.com>
2019-06-09 14:07:49 -0300

    [STYLE] Optimize imports

--- src/main/kotlin/costavictor/com/github/springresiliencycircuitbreaker/ImdbClient.kt
+++ src/main/kotlin/costavictor/com/github/springresiliencycircuitbreaker/ImdbClient.kt
@@ -2,7 +2,6 @@ package costavictor.com.github.springresiliencycircuitbreaker
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @FeignClient(
         name = "imdbApi",
